27669dca135147187ea49f9cd8166d7ad0f4b698
Jake Moretz <jmoretz9090@gmail.com>
2023-04-07 22:58:12 -0400

    able to log user info, still a good bit more to do

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
 
-// birth dates
-let birthYear = document.getElementById("#year").value;
-let birthMonth = document.getElementById("month").value;
-let birthDay = document.getElementById("day").value;
+// // birth dates
+// let birthYear = document.querySelector("#userYear").value;
+// let birthMonth = document.getElementById("month").value;
+// let birthDay = document.getElementById("day").value;
 
-// storing the current date
-let date = new Date();
-let currentDay = date.getDate();
-let currentMonth = date.getMonth();
-let currentYear = date.getFullYear();
+// // storing the current date
+// let date = new Date();
+// let currentDay = date.getDate();
+// let currentMonth = date.getMonth();
+// let currentYear = date.getFullYear();
+
+// //output
+
+// let d = currentDay - birthDay;
+// let m = currentMonth - birthMonth;
+// let y = currentYear - birthYear;
+
+
+// const input = document.querySelector("input");
+// const button = document.getElementById("btn1");
+// let output1 = document.querySelector("#currentYearData");
+// let output2 = document.querySelector("#currentMonthData");
+
+
+
+
+// button.addEventListener("click", function () {
+//     console.log(birthYear);
+// })
+    
+    
+    
+    // document.querySelector("currentDayData").textContent = "hello";
+    // document.querySelector("currentDayData").textContent = d;
+    // document.querySelector("currentYearData").textContent = "hello";
+
+
+
+
+
+
+
 // let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 // if (birthDay > currentDay){
@@ -20,15 +52,22 @@ let currentYear = date.getFullYear();
 //     currentYear = currentYear - 1;
 // }
 
-let d = currentDay - birthDay;
-let m = currentMonth - birthMonth;
-let y = currentYear - birthYear;
-
-const input = document.querySelector("input");
-const resultDay = document.querySelector("currentDayData");
-const resultMonth = document.querySelector("currentMonthData");
-const resultYear = document.querySelector("currentYearData");
 
 
-console.log(currentDay);
+// this works
+let button = document.querySelector('#btn1');
+let userBirthYear = document.querySelector("#userYear");
+let birthYear = ''
+
+function year() {
+    bYear = userBirthYear.value;
+}
+
+userBirthYear.addEventListener('input', year) 
+
+function myFunction () {
+    console.log(bYear);
+}
+
+button.addEventListener("click", myFunction);
 
